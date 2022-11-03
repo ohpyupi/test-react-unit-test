973d2065279a024cd6bc861e3d6633c74250660f
Hoseong Lee <hoseong.a.lee@gmail.com>
2022-11-02 21:45:00 -0700

    init commit

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		app: './app/app.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, './dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react'],
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					'html-loader',
+				],
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				],
+			},
+			{
+				test: /\.(sass|scss)$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				],
+			},
+			{
+				test: /\.(eot|svg|gif|png|jpg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+				use: [
+					'url-loader'
+				],
+			},
+		],
+	},
+};
---
+++ webpack.prod.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'), 
+    }),
+  ],
+  optimization: {
+    minimize: true,
+  },
+});
